c2fa7187792d1c73391489cba397284ad96e75f0
user <user@users-MacBook-Pro-107.local>
2018-10-27 13:15:25 +0100

    adds click on image functionality to reveal animal name

--- vocab_app/client/src/views/vocab_item_view.js
+++ vocab_app/client/src/views/vocab_item_view.js
@@ -6,7 +6,7 @@ const VocabItemView = function (container) {
 
 VocabItemView.prototype.bindEvents = function () {
   PubSub.subscribe('Vocab:data-retrieved', (event) => {
-    this.renderItem(event.detail[0]);
+    this.renderItem(event.detail[1]);
 
   }
 )
@@ -17,11 +17,46 @@ VocabItemView.prototype.renderItem = function (vocabItem) {
   itemDiv.id = vocabItem._id;
   itemDiv.classList.add('item-div')
 
+  const image = this.createImage(vocabItem);
+  itemDiv.appendChild(image);
 
-
+  const name = this.createName(vocabItem);
+  itemDiv.appendChild(name);
 
   this.container.appendChild(itemDiv);
 };
 
+VocabItemView.prototype.createName = function (vocabItem) {
+  const name = document.createElement('p');
+  name.classList.add('vocab-item-name')
+  name.id = `name-${vocabItem._id}`;
+  name.style.display = "none";
+  name.textContent = vocabItem.name
+  return name;
+};
+
+VocabItemView.prototype.createImage = function (vocabItem) {
+  const image = document.createElement('img');
+  image.src = vocabItem.image
+  image.id = `img-${vocabItem._id}`;
+  image.classList.add('vocab-item-image')
+  this.clickImage(image, vocabItem)
+  return image;
+};
+
+VocabItemView.prototype.clickImage = function (image, vocabItem) {
+  image.addEventListener('click', () => {
+    const nameStyleSetting = document.querySelector(`#name-${vocabItem._id}`)
+    if (nameStyleSetting.style.display === "none") {
+      nameStyleSetting.style.display = "block"
+
+    }
+      else {
+        nameStyleSetting.style.display = "none"
+      }
+
+  })
+
+};
 
 module.exports = VocabItemView;
